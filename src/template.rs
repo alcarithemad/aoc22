069e8071d937865280967ba3912a08502c1f0e14
Colin Valliant <alcarithemad@gmail.com>
2022-12-05 21:40:32 -0800

    Update template.rs

--- src/template.rs
+++ src/template.rs
@@ -3,6 +3,5 @@ use aoc22::get_input;
 #[tokio::main]
 pub async fn main() -> Result<(), anyhow::Error> {
     let input_data = get_input!();
-    compile_error!("copy template.rs to dayN.rs and replace this line");
     Ok(())
 }
